364e731fbfe89045f6197f52a26ac409f32c66ef
espagnetamandine <amandine.espagnet33@gmail.com>
2026-06-10 15:42:59 +0200

    petites modifications rapides

--- moderne_cpp/CJeu.cpp
+++ moderne_cpp/CJeu.cpp
@@ -32,12 +32,12 @@ void CJeu::JEU_JouerPartie() {
 			
 			while (uiJEU_IdJoueurCourrant != vjJEU_joueurs.size()) // pli 
 			{
-				carte = vjJEU_joueurs[uiJEU_IdJoueurCourrant]->JOUEUR_Choix_Carte_A_Jouer();
+				carte = vjJEU_joueurs[uiJEU_IdJoueurCourrant]->JOU_choixCarteAJouer();
 
-				if (!prJEU_strategieRegle->REG_CarteValide(*carte)) { /*erreur*/ }
+				if (!prJEU_strategieRegle->REG_CarteValide(*carte)) { } //erreur 
 				else
 				{
-					pJEU_pli.PAQ_AjouterCarte(carte);
+					pJEU_pli->PAQ_AjouterCarte(move(carte));
 					vuJEU_idJoueurPli.push_back(uiJEU_IdJoueurCourrant);
 					uiJEU_IdJoueurCourrant++;
 				}
@@ -51,42 +51,30 @@ void CJeu::JEU_JouerPartie() {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-void CJeu::JEU_JouerPartie() {
-	unique_ptr<CCarte> carte;
-	prJEU_strategieRegle->REG_DistribuerCartes(vjJEU_joueurs, move(ppJEU_paquetDeCartes));
-	while (!prJEU_strategieRegle->REG_FinDePartie()) // partie
-	{
-		while (!prJEU_strategieRegle->REG_FinDeManche()) // manche
-		{
-			while (uiJEU_IdxJoueurCourrant != vjJEU_joueurs.size()) // pli 
-			{
-				carte = vjJEU_joueurs[uiJEU_IdxJoueurCourrant]->JOUEUR_Choix_Carte_A_Jouer();
-
-				if (uiJEU_IdxJoueurCourrant == 0 && !prJEU_strategieRegle->REG_PremiereCarte(*carte)) { /* erreur*/ }
-
-				else if (!prJEU_strategieRegle->REG_CarteValide(*carte)) { /*erreur*/ }
-
-				else
-				{
-					mJEU_pli[vjJEU_joueurs[uiJEU_IdxJoueurCourrant].get()] = move(carte);
-					prJEU_strategieRegle->REG_GagnePli(mJEU_pli); //calcule les points 
-				}
-			}
-		}
-		prJEU_strategieRegle->REG_AfficherPoints(); // points de la manche 
-	}
-	prJEU_strategieRegle->REG_AfficherPoints(); // points de la partie
-	prJEU_strategieRegle->REG_AfficherGagnant();
-}
+//void CJeu::JEU_JouerPartie() {
+//	unique_ptr<CCarte> carte;
+//	prJEU_strategieRegle->REG_DistribuerCartes(vjJEU_joueurs, move(ppJEU_paquetDeCartes));
+//	while (!prJEU_strategieRegle->REG_FinDePartie()) // partie
+//	{
+//		while (!prJEU_strategieRegle->REG_FinDeManche()) // manche
+//		{
+//			while (uiJEU_IdxJoueurCourrant != vjJEU_joueurs.size()) // pli 
+//			{
+//				carte = vjJEU_joueurs[uiJEU_IdxJoueurCourrant]->JOUEUR_Choix_Carte_A_Jouer();
+//
+//				if (uiJEU_IdxJoueurCourrant == 0 && !prJEU_strategieRegle->REG_PremiereCarte(*carte)) { /* erreur*/ }
+//
+//				else if (!prJEU_strategieRegle->REG_CarteValide(*carte)) { /*erreur*/ }
+//
+//				else
+//				{
+//					mJEU_pli[vjJEU_joueurs[uiJEU_IdxJoueurCourrant].get()] = move(carte);
+//					prJEU_strategieRegle->REG_GagnePli(mJEU_pli); //calcule les points 
+//				}
+//			}
+//		}
+//		prJEU_strategieRegle->REG_AfficherPoints(); // points de la manche 
+//	}
+//	prJEU_strategieRegle->REG_AfficherPoints(); // points de la partie
+//	prJEU_strategieRegle->REG_AfficherGagnant();
+//}
